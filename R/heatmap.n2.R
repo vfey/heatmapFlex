7209788ada9f7324d741111aec6aa405d4c769e8
Vidal Fey <vidal.fey@tuni.fi>
2021-08-19 21:53:05 +0300

    updated *ignore files; added LIVENSE file; small change in documentation

--- R/heatmap.n2.R
+++ R/heatmap.n2.R
@@ -1,8 +1,8 @@
 #' @title Tools to Generate Flexible Heatmaps
 #' @docType package
 #' @name heatmapFlex
-#' @description The package has a number of tools supporting more flexible heatmaps. The graphics is grid-like using
-#'  the old graphics system. The main function is \code{heatmap.n2}, which is a wrapper around the various functions
+#' @description A set of tools supporting more flexible heatmaps. The graphics is grid-like using
+#'  the old graphics system. The main function is \code{heatmap.n2()}, which is a wrapper around the various functions
 #'  constructing individual parts of the heatmap, like sidebars, picket plots, legends etc. The function supports zooming
 #'  and splitting, i.e., having (unlimited) small heatmaps underneath each other in one plot deriving from the same data set,
 #'  e.g., clustered and ordered by a supervised clustering method.
